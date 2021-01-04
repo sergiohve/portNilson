b507b8962733eea89775fec532dce7a26452ae29
Sergio Herrera <sergioh24118642@gmail.com>
2021-01-04 15:45:41 -0400

    Updates

---
+++ precache-manifest.2e75ba308b399e38c259e24fd4b38d65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "937e21f34ac74b4c8038",
+    "url": "/portNilson/static/css/main.680f5a07.chunk.css"
+  },
+  {
+    "revision": "937e21f34ac74b4c8038",
+    "url": "/portNilson/static/js/main.937e21f3.chunk.js"
+  },
+  {
+    "revision": "8ea69148f77fef74db9a",
+    "url": "/portNilson/static/js/runtime~main.8ea69148.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portNilson/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "b8b78688c00044b8c0f161109cbd84c1",
+    "url": "/portNilson/index.html"
+  }
+];
